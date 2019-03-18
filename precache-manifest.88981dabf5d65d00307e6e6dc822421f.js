412e69d8f98b5ab4482868313b0fd262e65f92e0
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2019-03-18 20:58:57 -0300

    Updates

---
+++ precache-manifest.88981dabf5d65d00307e6e6dc822421f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "095c0811941b2e9fea0858376c789efb",
+    "url": "/portfolio/static/media/imagePresentation.095c0811.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2642ab6925efb5bc8842",
+    "url": "/portfolio/static/js/main.5ed05526.chunk.js"
+  },
+  {
+    "revision": "4af0859f8bfd39df83ec",
+    "url": "/portfolio/static/js/2.e4286200.chunk.js"
+  },
+  {
+    "revision": "2642ab6925efb5bc8842",
+    "url": "/portfolio/static/css/main.a0aed4d4.chunk.css"
+  },
+  {
+    "revision": "842be8a2e8c6eaacb46217a24c817388",
+    "url": "/portfolio/index.html"
+  }
+];
