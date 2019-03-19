36075e54cc53de83729e4dc3a0ec9808b8be184e
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2019-03-18 22:55:52 -0300

    Updates

---
+++ precache-manifest.68865474375b5033c0dd2ff8bf6154b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c5ac94341baaf6a6c4e4289014b6ede",
+    "url": "/portfolio/static/media/perfil.8c5ac943.jpg"
+  },
+  {
+    "revision": "095c0811941b2e9fea0858376c789efb",
+    "url": "/portfolio/static/media/imagePresentation.095c0811.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "decbd9517eabfe27b07a",
+    "url": "/portfolio/static/js/main.46dc79c3.chunk.js"
+  },
+  {
+    "revision": "2131e331929c8b7ef41e",
+    "url": "/portfolio/static/js/2.8746b762.chunk.js"
+  },
+  {
+    "revision": "decbd9517eabfe27b07a",
+    "url": "/portfolio/static/css/main.a0aed4d4.chunk.css"
+  },
+  {
+    "revision": "f0d1e99f5a9e12e8b44cac12e227c33f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.88981dabf5d65d00307e6e6dc822421f.js"
+  "/portfolio/precache-manifest.68865474375b5033c0dd2ff8bf6154b7.js"
 );
 
 workbox.clientsClaim();
